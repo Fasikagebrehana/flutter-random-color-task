f9dcf848f0da92692a94ba78806ddcb782b8df3c
Fasikagebrehana <fasikagebrehana1@gmail.com>
2025-07-16 10:09:27 +0300

    Refactor main.dart to update app title and improve ColorChanger widget functionality by applying DRY and KISS principles and add color_utils.dart for random color generation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'utils/color_utils.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,50 +9,82 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Test Task',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const ColorChanger(),
-    debugShowCheckedModeBanner: false,    
+      debugShowCheckedModeBanner: false,
     );
-  
-  }}
-  class ColorChanger extends StatefulWidget {
-    const ColorChanger({super.key});
-  
-    @override
-    State<ColorChanger> createState() => _ColorChangerState();
   }
-  
-  class _ColorChangerState extends State<ColorChanger> {
-    Color _backgroundColor = Colors.white;
+}
 
-    void _changeColor() {
-      final random = Random();
-      setState(() {
-        _backgroundColor = Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
-      });
-    }
+//  A stateful widget that displays "Hello there" and changes the background color on tap.
+class ColorChanger extends StatefulWidget {
+  const ColorChanger({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return GestureDetector(
-        onTap: _changeColor,
-        child: Scaffold(
-            backgroundColor: _backgroundColor,
-            body: Center(
-              child: const Text(
-                'Hello There',
-                style: TextStyle(fontSize: 24, color: Colors.black),
+  @override
+  State<ColorChanger> createState() => _ColorChangerState();
+}
+
+class _ColorChangerState extends State<ColorChanger> {
+  Color _backgroundColor = Colors.white;
+  int _tapCount = 0;
+
+  void _changeColor() {
+    setState(() {
+      _backgroundColor = generateRandomColor();
+      _tapCount++;
+    });
+  }
+
+  Color _getTextColor() {
+    final brightnessLevel = _backgroundColor.computeLuminance();
+    return brightnessLevel > 0.5 ? Colors.black : Colors.white;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _changeColor,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        color: _backgroundColor,
+        child: Stack(
+          children: [
+            Center(
+              child: Text(
+                'Hello there',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: _getTextColor(),
+                  shadows: const [
+                    Shadow(
+                      blurRadius: 2.0,
+                      color: Colors.grey,
+                      offset: Offset(1.0, 1.0),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        
-      );
-    }
+            Positioned(
+              top: 16,
+              right: 16,
+              child: Text(
+                'Taps: $_tapCount',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: _getTextColor(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
